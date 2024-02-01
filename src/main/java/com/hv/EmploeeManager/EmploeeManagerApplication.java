39c337d553c7fdfb9a1a6dcbbff43dd8824fc8a7
Harish V <harishhdvp1623@gmail.com>
2024-02-01 09:16:49 +0530

    Initial Project Setup

---
+++ src/main/java/com/hv/EmploeeManager/EmploeeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.hv.EmploeeManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploeeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmploeeManagerApplication.class, args);
+	}
+
+}
